7fe7654233ddd365907ff357b185e5662ac2b1e0
Nevedomo <chedser@gmail.com>
2023-08-05 14:41:13 +0300

    Update StudyWork8.cpp

--- StudyWork8/StudyWork8.cpp
+++ StudyWork8/StudyWork8.cpp
@@ -12,62 +12,46 @@
 
 #include "IOManager_h.h"
 
-using namespace Morozov;
+using namespace Vectorflex;
 
 int main(int argc, char** argv){
-
 setlocale(LC_ALL, "");
 
 string greetings = "**********************************************************\n\
 * Нижегородский Государственный Технический Университет *\n\
 * Лабораторная работа №8. Вариант 7\n\
-* Выполнил студент группы 21 - ИВТз Морозов Олег\n\
 * ****************************************************************\n";
 
 cout << greetings;
-
 if(argc < 2){
-
 	cout << "Запуск без аргументов\n";
 	cout << IOManager::GetManual();
 	exit(1); 
-
 }
 
 if(strcmp(argv[1],"-h") == 0 || strcmp(argv[1],"-help") == 0){
-
 	cout << IOManager::GetManual();
 	exit(0);
-
 }
 
-
-
 if(argc == 4){
-
 	string recordsCountStr = argv[2];
-
 		if(!IOManager::IsDigit(recordsCountStr)){
-			
 			cout << "Неизвестная команда\n";
 			cout << IOManager::GetManual();			
 			exit(1);
 		}
 
 	int recordsCount = stoi(recordsCountStr);
-
 	string commandType = argv[1];
 	string fileName = argv[3];
 
-
 		if(commandType == "-c"){
-		
 		IOManager::CreateRecords(fileName, recordsCount);
 		exit(0);
 		}
 
 		if(commandType == "-r"){
-
 		IOManager::PrintRecords(fileName, recordsCount);
 		exit(0);
 		}
@@ -75,19 +59,15 @@ if(argc == 4){
 	cout << "Неизвестная комманда\n";
 	cout << IOManager::GetManual();
 	exit(1);
-
-
 }
 
 cout << "Неизвестная комманда\n";
 cout << IOManager::GetManual();
 exit(1);
 
-
 /****************************************************************************/
 
 /*
-
 // ЧТЕНИЕ
 /* ifstream fin("someFile", std::ios::in | std::ios::binary);
 
@@ -112,7 +92,5 @@ cout << "Дата\n";
 cout << ticketCashier->date << '\n';
 
 fin.close();*/ 
-
 return 0;
-
 }
