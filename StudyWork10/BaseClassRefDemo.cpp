9b6e261ad70e1b55d1f5e0cae95109337827cb0d
Nevedomo <chedser@gmail.com>
2023-08-05 14:53:58 +0300

    Update BaseClassRefDemo.cpp

--- StudyWork10/BaseClassRefDemo.cpp
+++ StudyWork10/BaseClassRefDemo.cpp
@@ -5,7 +5,7 @@
 * Резюме : Пример программы, написанной на языке программирования C++
 * Описание : Разрабатываемая программа предназначена для хранения массива переменных структурного типа данных(записей)
 * Дата создания : 2022/02/04
-* Автор : Morozov Oleg
+* Автор : Vectorflex
 * Платформа : OS Linux Mint
 */
 
@@ -25,30 +25,29 @@ class TriodeTestParent{
     TriodeTestParent(float voltage, float resistance, int frequency, float b){
     cout << "Вызов конструктора базового класса TriodeTestParent\n";
         this->voltage = voltage;
-    this->resistance = resistance;
-    this->frequency = frequency;
-    this->b = b;}
+    	this->resistance = resistance;
+   	 this->frequency = frequency;
+    	this->b = b;
+    }
     
    virtual ~TriodeTestParent(){
- cout << "Вызов деструктора базового класса TriodeTestParent\n";}
-
+ 	cout << "Вызов деструктора базового класса TriodeTestParent\n";
+   }
 };
 
 class TriodeTestChild:public TriodeTestParent{
      public:    
-    TriodeTestChild(float voltage, float resistance, int frequency, float b) :TriodeTestParent(voltage, resistance, frequency, b){
-    cout << "Вызов конструктора базового класса TriodeTestChild\n";}
+    		TriodeTestChild(float voltage, float resistance, int frequency, float b) :TriodeTestParent(voltage, resistance, frequency, b){
+    			cout << "Вызов конструктора базового класса TriodeTestChild\n";
+		}
  virtual ~TriodeTestChild(){
-cout << "Вызов деструктора базового класса TriodeTestChild\n";}
-
+	cout << "Вызов деструктора базового класса TriodeTestChild\n";}
 };
 
 int main(int argc, char** argv){
 	setlocale(LC_ALL, "");
     
     TriodeTestChild child(220, 10, 50, 20);
-
     TriodeTestParent* parent = &child; 
-   
-	return 0;
+  	return 0;
 }
