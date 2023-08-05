2fd00e7e46fb035742e3203ad9f4ee1c2765e3f9
Nevedomo <chedser@gmail.com>
2023-08-05 14:34:17 +0300

    Update sw7_h.h

--- StudyWork7/sw7_h.h
+++ StudyWork7/sw7_h.h
@@ -25,7 +25,6 @@ int GetRecordsCount(string); // Получить количество запис
 vector<string> SplitString(string,string); //Разделение записи
 
 string GetManual(){
-
     string manual = "Поддерживаемые опции запуска:\n\
     -help или -h - запуск программы в режиме получения справки\n\
     -c [N] [file_name] - запуск программы в режиме создания электронной\n\
@@ -35,39 +34,28 @@ string GetManual(){
     текстового файла file_name\n";
 
     return manual;
-
 }
 
 bool IsDigit(string str){
-
     if(str.size() == 0){return false;}
 
     bool isDigit = true;
-
     for(char s : str){
-
         if(!isdigit(s)){
-
             isDigit = false;
             break;
-
         }
-
     }
 
     return isDigit;
-
 }
 
 void CreateRecords(int recordsCount, string fileName){
-
     ofstream fout(fileName, std::ios::app);
 
     if(!fout.is_open()){
-
         cout << "Ошибка открытия файла\n";
         exit(1);
-
     }
 
     cout << "Количество записей для ввода " << recordsCount << "\n";
@@ -78,17 +66,14 @@ void CreateRecords(int recordsCount, string fileName){
     unsigned int revenue; //Суммарная выручка
     string date; // Дата продаж
 
-
     for(int i = 0; i < recordsCount; i++){
-
         cout << "\nВведите запись " << i + 1 << "\n";
-
         cout << "Введите номер кассы\n";
         cin >> number;
         fout << number << ",";
 
-           cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-         cout << "Введите Ф.И.О. кассира\n";
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        cout << "Введите Ф.И.О. кассира\n";
         getline(cin, initialsCashier);
 
         fout << initialsCashier << ",";
@@ -105,30 +90,23 @@ void CreateRecords(int recordsCount, string fileName){
         cout << "Введите дату\n";
         cin >> date;
         fout << date << "\n";
-
     }
 
     fout.flush();
     fout.close();
-
 }
 
 void PrintRecords(int  recordsCount, string fileName){
-
     ifstream fin(fileName);
 
     if(!fin.is_open()){
-
         cout << "Ошибка открытия файла\n";
         exit(1);
-
     }
 
     if(FileIsEmpty(fileName)){ //Проверка файла на пустоту
-
         cout << "Файл пуст\n";
         exit(1);
-
     }
 
     int recordsCountInFile = GetRecordsCount(fileName); //  Количество записей в файле
@@ -136,9 +114,7 @@ void PrintRecords(int  recordsCount, string fileName){
 
      // Если  пользователь запросил больше чем надо записей или не больше нуля
     if((recordsCountToPrint > recordsCountInFile) || recordsCountToPrint <= 0){
-
         recordsCountToPrint = recordsCountInFile;
-
     }
 
     int currentRecordsCount = 0; //Счетчик для записей
@@ -146,81 +122,56 @@ void PrintRecords(int  recordsCount, string fileName){
     string str;
 
     while(currentRecordsCount != recordsCountToPrint){
-
     getline(fin, str);
 
     vectorStr = SplitString(str, ",");
-
     for(string str:vectorStr){cout << str << " ";}
-
     cout << "\n";
     ++currentRecordsCount;
     }
-
-
 }
 
 bool FileIsEmpty(string fileName){
-
     ifstream fin(fileName);
 
     if(!fin.is_open()){throw std::runtime_error("Ошибка открытия файла");}
-
     int symCount = 0;
     char c;
 
     while(!fin.eof()){
-
         fin >> c;
         ++symCount;
-
     }
-
     fin.close();
-
     return (symCount == 1 || symCount == 0)?true:false;
-
 }
 
 int GetRecordsCount(string  fileName){
-
     ifstream fin(fileName);
 
     if(!fin.is_open()){throw std::runtime_error("Ошибка открытия файла\n");}
-
     string str;
-
     int recordsCount = 0;
-
     while(getline(fin,str)){
-
         ++recordsCount;
-
     }
 
     fin.close();
-
     return recordsCount;
-
 }
 
 vector<string> SplitString(string str, string delimiter) {
-
     std::vector<std::string> splittedString;
     int startIndex = 0;
     int  endIndex = 0;
-    while ((endIndex = str.find(delimiter, startIndex)) < str.size())
-    {
+    while ((endIndex = str.find(delimiter, startIndex)) < str.size()){
         std::string val = str.substr(startIndex, endIndex - startIndex);
         splittedString.push_back(val);
         startIndex = endIndex + delimiter.size();
     }
-    if (startIndex < str.size())
-    {
+    if (startIndex < str.size()){
         std::string val = str.substr(startIndex);
         splittedString.push_back(val);
     }
     return splittedString;
-
 }
-
