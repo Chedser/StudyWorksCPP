e57f337d8e7985c8b6d9fea72106ee42ac04161d
Nevedomo <chedser@gmail.com>
2023-08-05 15:08:52 +0300

    Update IOManager_c.cpp

--- StudyWork11/IOManager_c.cpp
+++ StudyWork11/IOManager_c.cpp
@@ -1,9 +1,8 @@
 #include "IOManager_h.h"
 
-using namespace Morozov;
+using namespace Vectorlfex;
 
 string IOManager::GetManual() {
-
     string manual = "Поддерживаемые опции запуска:\n\
     -help или -h - справка\n\
     [N] - выбор рецепта кофе:\n\
@@ -12,12 +11,10 @@ string IOManager::GetManual() {
     3 - Melange\n";
 
     return manual;
-
 }
 
 
 bool IOManager::FileExists(string fileName) {
-
     bool isExist = false;
     std::ifstream fin(fileName.c_str());
 
@@ -25,12 +22,10 @@ bool IOManager::FileExists(string fileName) {
 
     fin.close();
     return isExist;
-
 }
 
 bool IOManager::FileIsEmpty(string fileName) {
-
-    bool isExist = false;
+   bool isExist = false;
 
     ifstream fin(fileName, std::ios_base::in); // Открываем файл для чтения
     if (!fin.is_open()) {
@@ -41,36 +36,23 @@ bool IOManager::FileIsEmpty(string fileName) {
     char c;
 
     while (!fin.eof()) {
-
         fin >> c;
         ++symCount;
-
     }
 
     fin.close();
-
     return (symCount == 1 || symCount == 0) ? true : false;
-
 }
 
 bool IOManager::IsDigit(string str) {
-
     if (str.empty()) { return false; }
 
     bool isDigit = true;
-
     for (char s : str) {
-
         if (!isdigit(s)) {
-
             isDigit = false;
             break;
-
         }
-
     }
-
     return isDigit;
-
 }
-
