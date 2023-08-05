2b13bfb9b696645582403e22f5bf6c83ed971bd5
Nevedomo <chedser@gmail.com>
2023-08-05 14:05:12 +0300

    Update StudyWork1.cpp

--- StudyWork1/StudyWork1.cpp
+++ StudyWork1/StudyWork1.cpp
@@ -27,16 +27,13 @@ float userA = 0.0f;
 float userX = 0.0f;
 double resultValues[] = {0.0, 0.0};
 
-/* Начало главной функции */
 int main() {
-
 	setlocale(LC_ALL, ""); // Вызов функции для отображения русских символов
 
 	/* Приветствие */
 	const  char* greeting = "***********************************************************\n\
 * Нижегородский государственный технический университет *\n\
 * Лабораторная работа №1. Задание 1. Вариант 7. *\n\
-* Выполнил студент группы 21 - ИВТз Морозов Олег *\n\
 * **********************************************************\n";
 	printf("%s",greeting);
 
@@ -54,24 +51,18 @@ int main() {
 	inputHandler = scanf("%f", &userX);
 	
 	if (inputHandler && userX == RIGHT_X) {// Если пользователь ввел число и число верное...
-
 		SetResultOfExpressions(userA, userX, resultValues); //.. вызывается функция решения выражений
 		printf("Y = %f: Z = %f \n", resultValues[0], resultValues[1]); // Отображение выражений пользователю
-
 	}
 	else { // В противном случае отображается сообщение об ошибке 
-
 		printf("Ошибка ввода\n");
-
 	}
 
 	return EXIT_SUCCESS; // Выход из главной функции
-
 } // Конец главной функции
 
 /* Определение функции решения выражений  */
 void SetResultOfExpressions(double a, double x, double* resultValues) {
-
 /* Цель: Решение выражений Y(A,X) и Z(A, X) */
 /* Исходные данные: 
  а – первая переменная 
@@ -89,5 +80,4 @@ void SetResultOfExpressions(double a, double x, double* resultValues) {
 	double Z = atan(sqrt(2*x + a));
 	resultValues[0] = Y;
         resultValues[1] = Z;
-
 }
