e9dc01fc6a25bd2461715c2fccb303c2338d13fd
Nevedomo <chedser@gmail.com>
2023-08-05 14:52:23 +0300

    Update TicketCashier_h.h

--- StudyWork9/TicketCashier_h.h
+++ StudyWork9/TicketCashier_h.h
@@ -1,22 +1,18 @@
 #ifndef TICKET_CASHIER_H
 #define TICKET_CASHIER_H
 
-
 #include "String_h.h"
 
 /* СТРУКТУРА АТРИБУТОВ КЛАССА TicketCashier*/
 struct TicketCashierInfo {
-
 	int officeNumber = 0; // Номер кассы
 	string fio; // ФИО кассира
 	int soldTicketsCout = 0; // Количество проданных билетов
 	float revenue; // Суммарная выручка
 	string date; // Дата
-
 };
 
 class TicketCashier {
-
 	TicketCashierInfo ticketCashierInfo;
 
 public:
@@ -43,29 +39,22 @@ public:
 
 	// Отладочное отображение информации
 	void PrintInfo();
-
 };
 
 TicketCashier::TicketCashier(int officeNumber, string fio, int soldTicketsCount, float revenue, string date) {
-
 	SetOfficeNumber(officeNumber);
 	SetFio(fio);
 	SetSoldTicketsCount(soldTicketsCount);
 	SetRevenue(revenue);
 	SetDate(date);
-	
-
 }
 TicketCashier::TicketCashier(int officeNumber, const char* fio, int soldTicketsCount, float revenue, const char* date) {
-
 	SetOfficeNumber(officeNumber);
 	SetFio(fio);
 	SetSoldTicketsCount(soldTicketsCount);
 	SetRevenue(revenue);
 	SetDate(date);
-
 }
-
 
 void TicketCashier::SetOfficeNumber(int officeNumber) {ticketCashierInfo.officeNumber = officeNumber;}
 void TicketCashier::SetFio(string fio) {ticketCashierInfo.fio = fio;}
@@ -82,11 +71,9 @@ float TicketCashier::GetRevenue() { return ticketCashierInfo.revenue; }
 string TicketCashier::GetDate() { return ticketCashierInfo.date; }
 
 void TicketCashier::PrintInfo() {
-
 	cout << "Номер кассы " << ticketCashierInfo.officeNumber << "\nФИО " << ticketCashierInfo.fio <<
 		"\nКоличество проданных билетов " << ticketCashierInfo.soldTicketsCout <<
          "\nСуммарная выручка " << ticketCashierInfo.revenue << 
 		"\nДата " << ticketCashierInfo.date << '\n';
-
 }
 #endif
