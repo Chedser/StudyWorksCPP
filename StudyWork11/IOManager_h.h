c0e23d13aa13e116f7a4fdf51304ef55f75b2756
Nevedomo <chedser@gmail.com>
2023-08-05 15:09:43 +0300

    Update IOManager_h.h

--- StudyWork11/IOManager_h.h
+++ StudyWork11/IOManager_h.h
@@ -1,27 +1,20 @@
 #ifndef IOMANAGER_H
 #define IOMANAGER_H
 
-
 #include <iostream>
 #include <fstream>
 
 using std::string;
 using std::ifstream;
 
-namespace Morozov {
+namespace Vectorflex {
 /* Вспомогательный класс для работы с файлами */    
 class IOManager {
-
-    public:  
-     
-        static string GetManual();
-        static bool FileExists(string fileName);
-        static  bool FileIsEmpty(string fileName);
-       static bool IsDigit(string str);
-
+        public:  
+            static string GetManual();
+            static bool FileExists(string fileName);
+            static  bool FileIsEmpty(string fileName);
+            static bool IsDigit(string str);
     };
-
 };
 #endif
-
-
