9b7a59813da6d5bd444941800c29846007039df8
Nevedomo <chedser@gmail.com>
2023-08-05 15:04:32 +0300

    Update StudyWork10.cpp

--- StudyWork10/StudyWork10.cpp
+++ StudyWork10/StudyWork10.cpp
@@ -25,84 +25,58 @@ int main(int argc, char** argv){
 * Выполнил студент группы 21 - ИВТз Vectorflex *\n\
 * ****************************************************************\n";
 
-	cout << greetings; 
-       
+    cout << greetings; 
+    
     try {
-
         if (argc < 2) {
-
             cout << "Запуск без аргументов\n";
-
             cout << IOM::GetManual();
-
             exit(1);
-
         }
 
         if (argc == 3) {
-        
-            cout << "Неверное количество аргументов\n";
-
+             cout << "Неверное количество аргументов\n";
             cout << IOM::GetManual();
-
             exit(1);
-
         }
 
         string commandType = argv[1]; // Тип команды
 
        if (commandType == "-h" || commandType == "-help") {
-         
             cout << IOM::GetManual();
             exit(0);
-
         }
 
        if (argc == 4 || argc == 5) {
-       
            string recordsCountStr = argv[2]; // Количество записей
            string fileName = argv[3]; // Название файла
            int recordsCount = stoi(recordsCountStr); // Количество записей в число
            
            if (commandType == "-c" && argc == 5) { // Команда создания записей
-
                string circuitElementTypeStr = argv[4];
-
                    if (!IOM::IsDigit(circuitElementTypeStr)) {
-               
-                       cout << "Неизвестная команда\n";
-
+                        cout << "Неизвестная команда\n";
                        cout << IOM::GetManual();
                        exit(1);
-
                    }
 
                int circuitElementType = stoi(circuitElementTypeStr); // Количество записей в число
-
                IOM::CreateRecords(recordsCount, fileName, ";", circuitElementType);
                exit(0);
            }
 
            if (commandType == "-r" && argc == 4) { // Команда создания записей
-
                IOM::PrintRecords(recordsCount, fileName, ";");
                exit(0);
            }
-
        }
 
         cout << "Неизвестная команда\n";
-
         cout << IOM::GetManual(); 
-
     }
     catch (const std::logic_error& str) {
-
         cout << "Неизвестная команда\n";
-
         cout << IOM::GetManual();
-
     }
-   
-	return 0;
+  	return 0;
 }
