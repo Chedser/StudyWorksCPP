e9480d92024c7ed40709b7808a92ed2bcfe6d593
Nevedomo <chedser@gmail.com>
2023-08-05 14:43:06 +0300

    Update TicketCashier_h.h

--- StudyWork8/TicketCashier_h.h
+++ StudyWork8/TicketCashier_h.h
@@ -1,25 +1,18 @@
 #ifndef TICKETCAHIER_H
 #define TICKETCASHIER_H
 
-
-namespace Morozov{
-
+namespace Vectorflex{
 	class TicketCashier{
-
 	public:
-
-	unsigned int officeNumber; // Номер кассы
-	char fio[80]; // Ф.И.О. кассира
-	unsigned int soldTicketsCount; //Количество проданных билетов
-	float revenue; // Cуммарная выручка
-	char date[10]; // Дата
+		unsigned int officeNumber; // Номер кассы
+		char fio[80]; // Ф.И.О. кассира
+		unsigned int soldTicketsCount; //Количество проданных билетов
+		float revenue; // Cуммарная выручка
+		char date[10]; // Дата
 
 	TicketCashier(){
 	}
 	~TicketCashier(){}
-
 	};
-
 }
-
 #endif
