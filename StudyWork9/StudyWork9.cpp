253321002c0f246e093732c6098e9fa7876ff773
Nevedomo <chedser@gmail.com>
2023-08-05 14:51:24 +0300

    Update StudyWork9.cpp

--- StudyWork9/StudyWork9.cpp
+++ StudyWork9/StudyWork9.cpp
@@ -4,7 +4,7 @@
 * Резюме : Пример программы, написанной на языке программирования C++
 * Описание : Разрабатываемая программа предназначена для хранения массива переменных структурного типа данных(записей)
 * Дата создания : 2022/02/03
-* Автор : Morozov Oleg
+* Автор : Vectorflex
 * Платформа : OS Linux Mint
 */
 
@@ -14,36 +14,25 @@
 typedef IOManager IOM;
 
 int main(int argc, char** argv){
- 
     setlocale(LC_ALL, "");
 
 	string greetings = "*********************************************\n\
 * Нижегородский государственный  технический университет *\n\
 * Лабораторная работа №9. Вариант 7. *\n\
-* Выполнил студент группы 21 - ИВТз Морозов Олег *\n\
 * ****************************************************************\n";
 
-	cout << greetings;
-	
+cout << greetings;
     try {
-
         if (argc < 2) {
-
             cout << "Запуск без аргументов\n";
-
             cout << IOM::GetManual();
-
             exit(1);
-
         }
 
         string commandType = argv[1]; // Тип команды
-
         if (commandType == "-h" || commandType == "-help") {
-
             cout << IOM::GetManual();
             exit(0);
-
         }
 
         string recordsCountStr = argv[2]; // Количество записей
@@ -51,37 +40,24 @@ int main(int argc, char** argv){
 
         //Пользователь ввел 4 аргумента
         if ((recordsCountStr.size() > 0 && fileName.size() > 0) && IOM::IsDigit(recordsCountStr)) {
-
             int recordsCount = stoi(recordsCountStr); // Количество записей в число
-
             if (commandType == "-c") { // Команда создания записей
-
                 IOM::CreateRecords(recordsCount, fileName, ";");
                 exit(0);
             }
 
             if (commandType == "-r") { // Команда чтения записей
-
                 IOM::PrintRecords(recordsCount, fileName, ";");
                 exit(0);
-
             }
-
         }
 
         cout << "Неизвестная команда\n";
-
         cout << IOM::GetManual();
-
     }
     catch (const std::logic_error& str) {
-
         cout << "Неизвестная команда\n";
-
         cout << IOM::GetManual();
-
     }
-
 	return 0; 
 }
-
